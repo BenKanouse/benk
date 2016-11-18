861dedf596696a6cbf88904db4af2a8bc4078a6f
BenKanouse <kanobt61@gmail.com>
2016-11-17 20:22:58 -0500

    Move nav to layout
    I plan to add more pages and it will be nice to have the nav in one
    spot.

--- public/js/freelancer.js
+++ public/js/freelancer.js
@@ -7,7 +7,7 @@
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 80)
+            scrollTop: ($($anchor.attr('href').substring(1)).offset().top - 80)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
